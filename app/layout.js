788b5269d2768c5f630367f2d964cbebe652332f
Alm0raa <ebrarakovaaa@gmail.com>
2026-09-15 14:09:45 +0300

    Delete app/layout.js

--- app/layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'Akdeniz Pati Haritası',description:'Akdeniz Üniversitesi kampüs mama noktaları'};
-export default function RootLayout({children}){return <html lang="tr"><body>{children}</body></html>}
